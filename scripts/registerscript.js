89bf8bcf45c154837b4103ae5b1cf74776fbd73f
Adam Zabenskie <zubs047@gmail.com>
2025-03-20 11:27:54 -0700

    changing forms

--- scripts/registerscript.js
+++ scripts/registerscript.js
@@ -2,21 +2,29 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("register-form");
     form.setAttribute("novalidate", true);
     form.addEventListener("submit", (e) => {
-        const inputemail = document.getElementById("register-email");
+        const inputname = document.getElementById("register-username");
         const inputpassword = document.getElementById("register-password");
-        const emailmsg = document.getElementById("email-error-message");
+        const inputemail = document.getElementById("register-email");
+        const namemsg = document.getElementById("name-error-message");
         const passmsg = document.getElementById("password-error-message");
-        if (inputemail.value == null || inputemail.value == "" || !inputemail.value.includes('@')) {
+        const emailmsg = document.getElementById("email-error-message");
+        if (inputname.value == null || inputname.value == "") {
             e.preventDefault();
-            emailmsg.textContent = "Must enter an email address";
+            namemsg.textContent = "Must enter a user name.";
         } else {
-            emailmsg.textContent = "";
+            namemsg.textContent = "";
         }
         if (inputpassword.value == null || inputpassword.value == "") {
             e.preventDefault();
-            passmsg.textContent = "Must enter a password";
+            passmsg.textContent = "Must enter a password.";
         } else {
             passmsg.textContent = "";
+        }
+        if (inputemail.value == null || inputemail.value == "" || !inputemail.value.includes('@')) {
+            e.preventDefault();
+            emailmsg.textContent = "Must enter an email address.";
+        } else {
+            emailmsg.textContent = "";
         }
     });
 })
